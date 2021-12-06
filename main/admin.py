835d8d2cfa997dcc112cbc0c408c33313de6fa81
Anthony Rivas <antho.snsd@gmail.com>
2021-12-06 00:23:37 -0500

    Tablas Brand, TypeProd y Product creadas y funcionando

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Brand, TypeProd, Product
+admin.site.register(Brand)
+admin.site.register(TypeProd)
+admin.site.register(Product)
 # Register your models here.
